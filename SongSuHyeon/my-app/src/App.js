16c74af7f40d8db9dc77962293589e768bdb4518
송수현 <ssh1692@naver.com>
2023-01-17 17:30:30 +0900

    Day 01/17 : Coin Tracker

--- SongSuHyeon/my-app/src/App.js
+++ SongSuHyeon/my-app/src/App.js
@@ -1,38 +1,29 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    console.log(toDo);
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
-  console.log(toDos);
-  return (
-    <div className="App">
-      <h3>My To Do List ({toDos.length})</h3>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Click</button>
-        <hr/>
-        <ul>
-          {toDos.map((item, index) => <li key={index}>{item}</li>)}
-        </ul>
-      </form>
-    </div>
-  );
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.coinpaprika.com/v1/tickers")
+        .then((response) => response.json())
+        .then(json => {
+            setCoins(json);
+            setLoading(false);
+        });
+    }, []);
+    return (
+        <div className="App">
+            <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+            {loading ? <strong>loading...</strong> : 
+                <select>
+                    {coins.map((coin) => <option>{coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD</option>)}
+                </select>
+            }
+            
+        </div>
+    );
 }
 
 export default App;

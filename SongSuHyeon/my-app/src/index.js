6118f98e6ebcb6a1ff590ad699c2d6ab0f4e35cd
송수현 <ssh1692@naver.com>
2023-01-17 17:38:07 +0900

    Day 01/17 : Add README.md

--- SongSuHyeon/my-app/src/index.js
+++ SongSuHyeon/my-app/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import AppToDos from './AppToDos';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppToDos />
   </React.StrictMode>
 );
 

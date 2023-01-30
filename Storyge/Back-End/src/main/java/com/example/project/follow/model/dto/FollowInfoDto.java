da9e887fd49c3631a1e2f9e98452705f5669f12f
두소원 <sowondoo@naver.com>
2023-01-31 01:27:04 +0900

    [BE] Fix: follow serviceImpl 수정

--- Storyge/Back-End/src/main/java/com/example/project/follow/model/dto/FollowInfoDto.java
+++ Storyge/Back-End/src/main/java/com/example/project/follow/model/dto/FollowInfoDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class FollowUserDto {
+public class FollowInfoDto {
 
     private Long followId;
     private Long following;
--- Storyge/Back-End/src/main/java/com/example/project/follow/model/service/FollowServiceImpl.java
+++ Storyge/Back-End/src/main/java/com/example/project/follow/model/service/FollowServiceImpl.java
@@ -1,11 +1,11 @@
 package com.example.project.follow.model.service;
 
-import com.example.project.follow.model.dto.FollowUserDto;
 import com.example.project.follow.model.entity.Follow;
 import com.example.project.follow.model.entity.FollowWait;
 import com.example.project.follow.model.repository.FollowRepository;
 import com.example.project.follow.model.repository.FollowWaitRepository;
 import com.example.project.user.model.dto.UserDto;
+import com.example.project.user.model.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,42 +21,60 @@ public class FollowServiceImpl implements FollowService {
     private final FollowWaitRepository followWaitRepository;
 
     @Override
-    public void insertFollowWait(long following) {
+    public void insertFollowWait(UserDto userDto) {
+        User currentUser = null;
+//        User following = userRepository.getOne(userDto.getUserId());
+        followWaitRepository.save(FollowWait.builder()
+                .userId(currentUser)
+//                .following(following)
+                .build());
+    }
+
+    @Override
+    public void insertFollower(UserDto userDto) {
+        User currentUser = null;
+//        User following = userRepository.getOne(userDto.getUserId());
+
+        followRepository.save(Follow.builder()
+//                .following(currentUser.getUserId())
+//                .follower(following)
+                .build());
 
     }
 
     @Override
-    public void insertFollower(long follower) {
+    public List<UserDto> selectAllFollowWait() {
 
-    }
+        Long currentUserId = (long)0;
+        List<FollowWait> followWaitList = followWaitRepository.findAll();
 
-    @Override
-    public List<UserDto> selectAllFollowWate(long userId) {
+
         return null;
     }
 
     @Override
-    public List<UserDto> selectAllFollowing(long userId) {
+    public List<UserDto> selectAllFollowing(UserDto userId) {
         return null;
     }
 
     @Override
-    public List<UserDto> selectAllFollower(long userId) {
+    public List<UserDto> selectAllFollower(UserDto userId) {
         return null;
     }
 
     @Override
-    public void deleteFollowWait(long waitId) {
+    public void deleteFollowWait(UserDto waitId) {
 
     }
 
     @Override
-    public void deleteFollowing(long followId) {
+    public void deleteFollowing(UserDto followId) {
 
     }
 
     @Override
-    public void deleteFollower(long followId) {
+    public void deleteFollower(UserDto followId) {
 
     }
 }
+

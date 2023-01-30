0e0f21afd03cc593496a62f20386b4135fc28ca7
안태현 <taehyunan@antaehyeon-ui-MacBookPro.local>
2023-01-30 21:04:40 +0900

    [FE] Design : 하단 nav바 생성

--- Storyge/front-end/src/common/footer/Nav.jsx
+++ Storyge/front-end/src/common/footer/Nav.jsx
@@ -1,44 +1,85 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import { TbHome, TbMusic, TbPlus, TbBell, TbUser } from "react-icons/tb";
-
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
+import * as S from "./NavStyle";
+import React from "react";
+import { useState } from "react";
 
 function Footer() {
+  const [home, setHome] = useState(true);
+  const [music, setMusic] = useState(false);
+  const [alert, setAlert] = useState(false);
+  const [user, setUser] = useState(false);
+
+  function goHome() {
+    setHome(true);
+    setMusic(false);
+    setAlert(false);
+    setUser(false);
+  }
+  function goMusic() {
+    setHome(false);
+    setMusic(true);
+    setAlert(false);
+    setUser(false);
+  }
+  function goWrite() {
+    setHome(false);
+    setMusic(false);
+    setAlert(false);
+    setUser(false);
+  }
+  function goAlert() {
+    setHome(false);
+    setMusic(false);
+    setAlert(true);
+    setUser(false);
+  }
+  function goUser() {
+    setHome(false);
+    setMusic(false);
+    setAlert(false);
+    setUser(true);
+  }
+
   return (
-    <>
-      <Navbar
-        bg="white"
-        variant="light"
-        fixed="bottom"
-        style={{
-          height: "6rem",
-          background: "#ffffff",
-          boxShadow: "0px 2px 15px rgba(184, 184, 210, 0.5)",
+    <S.Nav>
+      <S.IconContainer
+        onClick={() => {
+          goHome();
         }}
       >
-        <Container>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">
-              <TbHome className="icons" />
-            </Nav.Link>
-            <Nav.Link href="#music">
-              <TbMusic className="icons" />
-            </Nav.Link>
-            <Nav.Link href="#write">
-              <TbPlus className="icons" />
-            </Nav.Link>
-            <Nav.Link href="#alert">
-              <TbBell className="icons" />
-            </Nav.Link>
-            <Nav.Link href="#mypage">
-              <TbUser className="icons" />
-            </Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
+        <TbHome size={30} color={home ? "#ACCEBC" : "#D9D9D9"} />
+      </S.IconContainer>
+      <S.IconContainer
+        onClick={() => {
+          goMusic();
+        }}
+      >
+        <TbMusic size={30} color={music ? "#ACCEBC" : "#D9D9D9"} />
+      </S.IconContainer>
+      <S.IconContainer
+        onClick={() => {
+          goWrite();
+        }}
+      >
+        <S.CenterCircle>
+          <TbPlus size={30} color="white" />
+        </S.CenterCircle>
+      </S.IconContainer>
+      <S.IconContainer
+        onClick={() => {
+          goAlert();
+        }}
+      >
+        <TbBell size={30} color={alert ? "#ACCEBC" : "#D9D9D9"} />
+      </S.IconContainer>
+      <S.IconContainer
+        onClick={() => {
+          goUser();
+        }}
+      >
+        <TbUser size={30} color={user ? "#ACCEBC" : "#D9D9D9"} />
+      </S.IconContainer>
+    </S.Nav>
   );
 }
 
---
+++ Storyge/front-end/src/common/footer/NavStyle.js
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Nav = styled.div`
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  height: 66px;
+  width: 100%;
+  background: #ffffff;
+  box-shadow: 0px 2px 15px rgba(184, 184, 210, 0.5);
+`;
+
+export const IconContainer = styled.div`
+  height: 100%;
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CenterCircle = styled.div`
+  background-color: #accebc;
+  height: 50px;
+  width: 50px;
+  border-radius: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;

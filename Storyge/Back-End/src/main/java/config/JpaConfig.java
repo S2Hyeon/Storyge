e948cf6e3646ea0dce75942b313d3431c7c4b751
박수민 <1379psm@naver.com>
2023-01-30 10:32:59 +0900

    JpaConfig 어노테이션 추가

--- Storyge/Back-End/src/main/java/config/JpaConfig.java
+++ Storyge/Back-End/src/main/java/config/JpaConfig.java
@@ -1,4 +1,10 @@
 package config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
 public class JpaConfig {
+
 }

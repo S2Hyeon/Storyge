d33e467c02ac01a96cad79a4abe208e6d93408fd
두소원 <sowondoo@naver.com>
2023-01-31 01:27:04 +0900

    [BE] Fix: follow serviceImpl 수정

--- Storyge/Back-End/src/main/java/com/example/project/follow/model/service/FollowService.java
+++ Storyge/Back-End/src/main/java/com/example/project/follow/model/service/FollowService.java
@@ -1,32 +1,32 @@
 package com.example.project.follow.model.service;
 
 import com.example.project.follow.model.dto.FollowDto;
-import com.example.project.follow.model.dto.FollowUserDto;
 import com.example.project.follow.model.entity.Follow;
 import com.example.project.follow.model.entity.FollowWait;
 import com.example.project.user.model.dto.UserDto;
+import com.example.project.user.model.entity.User;
 
 import java.util.List;
 
 public interface FollowService {
 
     //follow 대기(following 신청)
-    void insertFollowWait(long following);
+    void insertFollowWait(UserDto following);
     // follow 등록(follower 수락)
-    void insertFollower(long follower);
+    void insertFollower(UserDto userDto);
 
     // 팔로우 대기 목록
-    List<UserDto> selectAllFollowWate(long userId);
+    List<UserDto> selectAllFollowWait();
     //팔로잉 목록
-    List<UserDto> selectAllFollowing(long userId);
+    List<UserDto> selectAllFollowing(UserDto userId);
     //팔로워 목록
-    List<UserDto> selectAllFollower(long userId);
+    List<UserDto> selectAllFollower(UserDto userId);
     //팔로우 대기 삭제
-    void deleteFollowWait(long waitId);
+    void deleteFollowWait(UserDto waitId);
     //팔로잉 삭제
-    void deleteFollowing(long followId);
+    void deleteFollowing(UserDto followId);
     //팔로우 삭제
-    void deleteFollower(long followId);
+    void deleteFollower(UserDto followId);
 
 
 }

b349982e0691cda13a9d02a288ca54c8bfe7af54
안태현 <taehyunan@antaehyeon-ui-MacBookPro.local>
2023-01-31 00:10:44 +0900

    [FE] Design : 명언 표시

--- Storyge/front-end/src/pages/2_main/MainStyle.js
+++ Storyge/front-end/src/pages/2_main/MainStyle.js
@@ -1,7 +1,48 @@
 import styled from "styled-components";
 
+export const All = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: #eaf2ea;
+`;
+
 export const NewDiary = styled.div`
-  height: 60px;
+  height: 62px;
   width: 100%;
   border: 1px solid red;
+  display: flex;
+  justify-content: center;
+
+  margin-top: 10px;
+  margin-bottom: 10px;
+  padding-left: 30px;
+
+  overflow-x: auto;
+  white-space: nowrap;
+`;
+
+export const Profile = styled.div`
+  height: 60px;
+  width: 60px;
+  border-radius: 100px;
+  border: 1px solid red;
+  flex-direction: column;
+  margin-left: 5px;
+  margin-right: 5px;
+  flex: 0 0 auto;
+`;
+
+export const Container = styled.div`
+  padding-left: 20px;
+  padding-right: 20px;
+  // background-color: #eaf2ea;
+`;
+
+export const WiseBox = styled.div`
+  width: 100%;
+  height: 108px;
+  background: #ffffff;
+  box-shadow: 0px 16px 40px -4px rgba(112, 144, 176, 0.2);
+  border-radius: 12px;
+  margin: 20px 0 20px 0;
 `;
---
+++ Storyge/front-end/src/pages/2_main/NewDiaryData.js
@@ -0,0 +1,54 @@
+let datas = [
+  {
+    id: 0,
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2016/03/23/04/01/woman-1274056__480.jpg",
+  },
+  {
+    id: 1,
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2015/06/22/08/40/child-817373__480.jpg",
+  },
+  {
+    id: 3,
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262__480.jpg",
+  },
+  {
+    id: 4,
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2015/01/08/18/24/children-593313__480.jpg",
+  },
+  {
+    id: 5,
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2017/10/20/10/58/elephant-2870777__480.jpg",
+  },
+  {
+    id: 6,
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2019/08/19/07/45/corgi-4415649__480.jpg",
+  },
+  {
+    id: 7,
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2015/11/17/13/13/bulldog-1047518__480.jpg",
+  },
+  {
+    id: 8,
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2016/11/19/09/45/man-1838330__480.jpg",
+  },
+  {
+    id: 9,
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2019/11/08/11/56/kitten-4611189__480.jpg",
+  },
+  {
+    id: 10,
+    imgUrl:
+      "https://cdn.pixabay.com/photo/2017/11/06/09/53/tiger-2923186__480.jpg",
+  },
+];
+
+export default datas;

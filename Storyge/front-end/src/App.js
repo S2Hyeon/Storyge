396c64f120101f871aae6b88fdcadd37a2cc0512
안태현 <dksxogus1012@gmail.com>
2023-01-31 11:09:04 +0900

    [FE] Style : 메인으로 라우팅 추가

--- Storyge/front-end/src/App.js
+++ Storyge/front-end/src/App.js
@@ -1,26 +1,24 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/1_login/Login';
-import LoginInfo from './pages/1_login/LoginInfo';
-import Intro from './pages/0_intro/Intro';
-import Music from './pages/8_music/Music';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/1_login/Login";
+import LoginInfo from "./pages/1_login/LoginInfo";
+import Intro from "./pages/0_intro/Intro";
+import Music from "./pages/8_music/Music";
 import Creatediary from "./pages/3_myDiary/Creatediary";
-
+import Main from "./pages/2_main/Main";
 
 function App() {
   return (
     <div className="App">
-      <h1>Intro</h1>
-
       <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Intro />} />
-            <Route path="login" element={<Login />} />
-            <Route path="logininfo" element={<LoginInfo />} />
-            <Route path="music" element={ <Music /> } />
-            <Route path="createDiary" element={<Creatediary />} />
-
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Intro />} />
+          <Route path="login" element={<Login />} />
+          <Route path="logininfo" element={<LoginInfo />} />
+          <Route path="music" element={<Music />} />
+          <Route path="createDiary" element={<Creatediary />} />
+          <Route path="main" element={<Main />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );

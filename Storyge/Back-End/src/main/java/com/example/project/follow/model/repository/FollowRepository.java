766438116d415e7f9a46b8364aca70b04947b2a5
두소원 <sowondoo@naver.com>
2023-01-31 11:35:34 +0900

    [BE] Fix: follower serviceImpl 수정

--- Storyge/Back-End/src/main/java/com/example/project/follow/model/repository/FollowRepository.java
+++ Storyge/Back-End/src/main/java/com/example/project/follow/model/repository/FollowRepository.java
@@ -3,6 +3,10 @@ package com.example.project.follow.model.repository;
 import com.example.project.follow.model.entity.Follow;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FollowRepository extends JpaRepository<Follow, Long> {
+import java.util.List;
 
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+    List<Follow> findByFollower(long follower);
+    List<Follow> findByFollowing(long following);
+    void deleteByFollowingAndFollower(long following, long follower);
 }
--- Storyge/Back-End/src/main/java/com/example/project/follow/model/repository/FollowWaitRepository.java
+++ Storyge/Back-End/src/main/java/com/example/project/follow/model/repository/FollowWaitRepository.java
@@ -1,10 +1,18 @@
 package com.example.project.follow.model.repository;
 
-import com.example.project.follow.model.dto.FollowUserDto;
 import com.example.project.follow.model.entity.FollowWait;
+import com.example.project.user.model.dto.UserDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface FollowWaitRepository extends JpaRepository<FollowWait, Long> {
+//    List<UserDto> findFollowWaitUserList(long userId);
+//    List<FollowWait> findByFollowing(long following);
 
+    List<FollowWait> findByFollowing(long following); //나의 팔로우 수락을 대기하는 사람들 리스트
 
+    FollowWait findByFollowingAndUserId(long following, long userId);
+    void deleteByFollowingAndUserId(long following, long userId);
 }

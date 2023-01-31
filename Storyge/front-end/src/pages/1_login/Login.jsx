bc2e37b8f04f695dec3c475271262a1ca0e3cfb9
송수현 <ssh1692@naver.com>
2023-01-31 10:57:10 +0900

    [FE] Design : login 버튼 디자인 수정

--- Storyge/front-end/src/pages/1_login/Login.jsx
+++ Storyge/front-end/src/pages/1_login/Login.jsx
@@ -5,27 +5,24 @@ import { FcGoogle } from "react-icons/fc";
 import { RiKakaoTalkFill } from "react-icons/ri";
 
 export default function Login() {
-
     return (
-        
         <div className='login'>
             <h1>Login</h1>
-            <button className='login__btn__facebook' onClick={() => alert("facebook")}>
-                <a href="#1">
-                    <AiFillFacebook />
-                    Facebook Login
-                </a>
-            </button>
 
-            <button className='login__btn__google' onClick={() => alert("google")}>
+            <a href="#!" className='login__btn__google' onClick={() => alert("google")}>
                 <FcGoogle />
                 Google Login
-            </button>
+            </a>
 
-            <button className='login__btn__kakao' onClick={() => alert("kakao")}>
+            <a href="#!" className='login__btn__kakao' type="button" onClick={() => alert("kakao")}>
                 <RiKakaoTalkFill />
                 Kakao Login
-            </button>
+            </a>
+            
+            <a href="#!" className='login__btn__facebook' type="button" onClick={() => alert("facebook")}>
+                <AiFillFacebook />
+                Facebook Login
+            </a>
         </div>
     );
 }

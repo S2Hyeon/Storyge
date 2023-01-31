8fd0e4314f6766fc295c9a1d5d3051d3534c41c9
박수민 <1379psm@naver.com>
2023-02-01 08:52:57 +0900

    [BE] Feat user 소셜 로그인 구조 생성 & config 설정

--- Storyge/Back-End/src/main/java/com/example/project/config/jwt/JwtAuthenticationFilter.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.config.jwt;
-
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-//로그인
-public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-
-
-}

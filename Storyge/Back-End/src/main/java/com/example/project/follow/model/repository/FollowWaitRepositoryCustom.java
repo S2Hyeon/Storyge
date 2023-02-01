a62c1256e2aa903d3f117dbe900f33871d03900e
두소원 <sowondoo@naver.com>
2023-02-01 15:12:10 +0900

    [BE] Fix: annotation 추가

--- Storyge/Back-End/src/main/java/com/example/project/follow/model/repository/FollowWaitRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.follow.model.repository;
-
-import com.example.project.follow.model.entity.FollowWait;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FollowWaitRepositoryCustom{
-
-}

117466091b86186f94e15b623d00b63fe1917f42
두소원 <sowondoo@naver.com>
2023-02-01 15:10:05 +0900

    [BE] Fix: annotation 추가

--- Storyge/Back-End/src/main/java/com/example/project/quote/model/repository/QuoteRepository.java
+++ Storyge/Back-End/src/main/java/com/example/project/quote/model/repository/QuoteRepository.java
@@ -2,7 +2,9 @@ package com.example.project.quote.model.repository;
 
 import com.example.project.quote.model.entity.Quote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuoteRepository extends JpaRepository<Quote, Long> {
 
 }

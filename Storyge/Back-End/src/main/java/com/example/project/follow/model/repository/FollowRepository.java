904448b8cd4e38bc972eb9ad9f38e1cba8f10a0a
두소원 <sowondoo@naver.com>
2023-02-01 15:12:10 +0900

    [BE] Fix: annotation 추가

--- Storyge/Back-End/src/main/java/com/example/project/follow/model/repository/FollowRepository.java
+++ Storyge/Back-End/src/main/java/com/example/project/follow/model/repository/FollowRepository.java
@@ -3,10 +3,11 @@ package com.example.project.follow.model.repository;
 import com.example.project.follow.model.entity.Follow;
 import com.example.project.user.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface FollowRepository extends JpaRepository<Follow, Long> {
     List<Follow> findByFollower(User follower);
     List<Follow> findByFollowing(User following);
--- Storyge/Back-End/src/main/java/com/example/project/follow/model/repository/FollowWaitRepository.java
+++ Storyge/Back-End/src/main/java/com/example/project/follow/model/repository/FollowWaitRepository.java
@@ -1,19 +1,19 @@
 package com.example.project.follow.model.repository;
 
 import com.example.project.follow.model.entity.FollowWait;
-import com.example.project.user.model.dto.UserDto;
 import com.example.project.user.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface FollowWaitRepository extends JpaRepository<FollowWait, Long> {
 //    List<UserDto> findFollowWaitUserList(long userId);
 //    List<FollowWait> findByFollowing(long following);
 
     List<FollowWait> findByFollowing(User following); //나의 팔로우 수락을 대기하는 사람들 리스트
-    FollowWait findByFollowingAndUserId(long following, long userId);
+//    FollowWait findByFollowingAndUserId(long following, long userId);
 
     //대기 상태 삭제
 //    void deleteByFollowingAndUserId(long following, long userId);

cb6136b3d3d0f97d762aa8154162f97d1184387e
안태현 <dksxogus1012@gmail.com>
2023-02-01 11:20:34 +0900

    [FE] Style : 전역 styled component style 추가

---
+++ Storyge/front-end/src/styles/index.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const BodyContainer = styled.div`
+  width: 100%;
+  padding: 0 20px;
+`;
+
+export const longBtnDefault = styled.btn`
+  background-color: var(--color-primary);
+  border-radius: 100px;
+  height: 48px;
+  width: 100%;
+  font-size: 18px;
+  border: hidden;
+`;
+
+export const longBtnDisabled = styled.btn`
+  background-color: var(--color-darkgrey);
+  color: var(--color-white);
+  border-radius: 100px;
+  height: 48px;
+  width: 100%;
+  font-size: 18px;
+  border: hidden;
+`;
+
+export const longBtnBorder = styled.btn`
+  border: hidden;
+  background-color: var(--color-white);
+  border-radius: 100px;
+  height: 48px;
+  width: 100%;
+  font-size: 18px;
+  border: 3px solid var(--color-primary);
+`;
+
+export const profileBox = styled.div``;

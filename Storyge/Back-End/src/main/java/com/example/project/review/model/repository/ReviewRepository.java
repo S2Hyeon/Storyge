0c1799149cf3d27de37caf39fbb01b1597769f9d
두소원 <sowondoo@naver.com>
2023-02-03 14:16:48 +0900

    [BE] Fix: review 버그 수정

--- Storyge/Back-End/src/main/java/com/example/project/review/model/repository/ReviewRepository.java
+++ Storyge/Back-End/src/main/java/com/example/project/review/model/repository/ReviewRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-    List<Review> findByDiaryId(Diary diaryId);
+    List<Review> findByDiaryIdOrderByCreatedAt(Diary diaryId);
 
 }

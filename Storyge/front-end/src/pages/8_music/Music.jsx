9cf26c9e0b18ec2c3f96ead0d6ae59411655c73c
서희연 <tjgmldus100@naver.com>
2023-02-03 16:08:59 +0900

    [FE] Feat : openAI 기능 생성

--- Storyge/front-end/src/pages/8_music/Music.jsx
+++ Storyge/front-end/src/pages/8_music/Music.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as G from "../../styles";
 import * as S from "./Music.js";
+import { OpenAI } from "../../openai/OpenAI";
 
 export default function Music() {
   const movePage = useNavigate();
+  const [result, setResult] = useState("뽕");
+
+  function getMusic() {
+    
+  }
 
   function gomusicresult() {
     movePage("/musicresult");
@@ -13,11 +19,17 @@ export default function Music() {
     <G.BodyContainer>
       <S.Rectangle placeholder="음악 추천을 받고 싶은 사연을 작성해주세요." />
       <G.longBtnDefault
-        onClick={gomusicresult}
+        onClick={() => {
+          setResult(OpenAI({ input: "기분이 너무 꿀꿀해요", type: 0 }));
+          console.log(
+            typeof OpenAI({ input: "기분이 너무 꿀꿀해요", type: 0 })
+          );
+        }}
         style={{ marginBottom: "20px" }}
       >
         <S.Text>분석하기</S.Text>
       </G.longBtnDefault>
+      <div>{result}</div>
     </G.BodyContainer>
   );
 }

8a2649b6553b851cbfed4a8bd273c7fbbc3bb6b3
송수현 <ssh1692@naver.com>
2023-02-03 16:34:42 +0900

    [FE] Fix : Music 분석 비동기 처리

--- Storyge/front-end/src/pages/8_music/Music.jsx
+++ Storyge/front-end/src/pages/8_music/Music.jsx
@@ -3,14 +3,14 @@ import { useNavigate } from "react-router-dom";
 import * as G from "../../styles";
 import * as S from "./Music.js";
 import { OpenAI } from "../../openai/OpenAI";
+// import { reject } from "q";
+// import { resolve } from "path";
 
 export default function Music() {
   const movePage = useNavigate();
-  const [result, setResult] = useState("뽕");
+  const [result, setResult] = useState("분석전");
 
-  function getMusic() {
-    
-  }
+  function getMusic() {}
 
   function gomusicresult() {
     movePage("/musicresult");
@@ -19,11 +19,9 @@ export default function Music() {
     <G.BodyContainer>
       <S.Rectangle placeholder="음악 추천을 받고 싶은 사연을 작성해주세요." />
       <G.longBtnDefault
-        onClick={() => {
-          setResult(OpenAI({ input: "기분이 너무 꿀꿀해요", type: 0 }));
-          console.log(
-            typeof OpenAI({ input: "기분이 너무 꿀꿀해요", type: 0 })
-          );
+        onClick={async () => {
+          setResult(await OpenAI({ input: "기분이 너무 꿀꿀해요", type: 0 }));
+          console.log("분석한 데이터 result -> " + result);
         }}
         style={{ marginBottom: "20px" }}
       >

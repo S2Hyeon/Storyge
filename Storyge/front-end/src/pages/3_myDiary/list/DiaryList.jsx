c520d6699b7731c7c99d7c0bd147a9509e65bd25
안태현 <taehyunan@antaehyeon-ui-MacBookPro.local>
2023-02-04 17:50:49 +0900

    [FE] Design : 일기 목록 디자인 수정

--- Storyge/front-end/src/pages/3_myDiary/list/DiaryList.jsx
+++ Storyge/front-end/src/pages/3_myDiary/list/DiaryList.jsx
@@ -1,54 +1,57 @@
 import React, { useState } from "react";
 import data from "./DiaryListData";
-import * as S from "../MyDiaryStyle";
-import * as A from "../../../styles/index";
-import { BsFillCaretLeftFill, BsFillCaretRightFill } from "react-icons/bs";
+import * as G from "../../../styles/index";
+import * as S from "./DiaryListStyle";
+import { TbChevronLeft, TbChevronRight } from "react-icons/tb";
 
 export default function Diarylist() {
-  // const [dateInfo, setDateInfo] = useState(new Date());
-  // const headDate = `${dateInfo.getFullYear()}.${
-  //   dateInfo.getMonth() + 1
-  // }.${dateInfo.getDate()}`;
-  // const decreaseDate = () => {
-  //   setDateInfo(
-  //     new Date(
-  //       dateInfo.getFullYear(),
-  //       dateInfo.getMonth(),
-  //       dateInfo.getDate() - 1
-  //     )
-  //   );
-  // };
-  // const increaseDate = () => {
-  //   setDateInfo(
-  //     new Date(
-  //       dateInfo.getFullYear(),
-  //       dateInfo.getMonth(),
-  //       dateInfo.getDate() + 1
-  //     )
-  //   );
-  // };
-  // return (
-  //   <A.BodyContainer>
-  //     <S.DiaryHeader>
-  //       <S.AutoBtn onClick={decreaseDate}>
-  //         <BsFillCaretLeftFill />
-  //       </S.AutoBtn>
-  //       <S.DateInfo>{headDate}</S.DateInfo>
-  //       <S.AutoBtn onClick={increaseDate}>
-  //         <BsFillCaretRightFill />
-  //       </S.AutoBtn>
-  //     </S.DiaryHeader>
-  //     {data.map((data) => {
-  //       return (
-  //         <S.ListBox key={data.id}>
-  //           <S.Emotion emotion={data.img} />
-  //           <S.Col>
-  //             <div>{data.time}</div>
-  //             <S.Content>{data.content}</S.Content>
-  //           </S.Col>
-  //         </S.ListBox>
-  //       );
-  //     })}
-  //   </A.BodyContainer>
-  // );
+  const [dateInfo, setDateInfo] = useState(new Date());
+  const headDate = `${dateInfo.getFullYear()}.${
+    dateInfo.getMonth() + 1
+  }.${dateInfo.getDate()}`;
+  const decreaseDate = () => {
+    setDateInfo(
+      new Date(
+        dateInfo.getFullYear(),
+        dateInfo.getMonth(),
+        dateInfo.getDate() - 1
+      )
+    );
+  };
+  const increaseDate = () => {
+    setDateInfo(
+      new Date(
+        dateInfo.getFullYear(),
+        dateInfo.getMonth(),
+        dateInfo.getDate() + 1
+      )
+    );
+  };
+  return (
+    // 날짜 표시 및 날짜이동 부분
+    <G.BodyContainer>
+      <S.DateContainer>
+        <S.ArrowBtn onClick={decreaseDate}>
+          <TbChevronLeft color="var(--color-primary)" />
+        </S.ArrowBtn>
+        <S.DateInfo>{headDate}</S.DateInfo>
+        <S.ArrowBtn onClick={increaseDate}>
+          <TbChevronRight color="var(--color-primary)" />
+        </S.ArrowBtn>
+      </S.DateContainer>
+
+      {/* 리스트 부분 */}
+      {data.map((data) => {
+        return (
+          <S.ListBox key={data.id}>
+            <S.Emotion emotion={data.img} />
+            <S.TimeSummaryContainer>
+              <S.Time>{data.time}</S.Time>
+              <S.Summary>{data.content}</S.Summary>
+            </S.TimeSummaryContainer>
+          </S.ListBox>
+        );
+      })}
+    </G.BodyContainer>
+  );
 }

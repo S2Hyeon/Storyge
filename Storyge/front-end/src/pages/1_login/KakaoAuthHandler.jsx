94c91d050076b6ce1a58cfe163b018a06e399d3c
박수민 <1379psm@naver.com>
2023-02-06 15:54:10 +0900

    [FE] Test : Kakao Login 백엔드 연결

--- Storyge/front-end/src/pages/1_login/KakaoAuthHandler.jsx
+++ Storyge/front-end/src/pages/1_login/KakaoAuthHandler.jsx
@@ -1,20 +1,32 @@
-import React from "react";
-import { useDispatch } from "react-redux";
-import { actionCreators as userActions } from "./../../redux/modules/user";
-import Spinner from "./Spinner";
+// import React, { useEffect } from "react";
+// import { actionCreators as userActions } from "./../../redux/modules/user";
+// import Spinner from "./Spinner";
 
-const KaKaoLoginHandler = (props) => {
-  //Dispatch: store에 있는 데이터를 컨트롤 하기 위함
-  const dispatch = useDispatch();
-  //인가 코드
-  let code = new URL(window.location.href).searchParams.get("code");
-  console.log(code);
+// const KaKaoLoginHandler = (props) => {
+//   //Dispatch: store에 있는 데이터를 컨트롤 하기 위함
+//   //인가 코드
 
-  React.useEffect(async () => {
-    await dispatch(userActions.kakaoLogin(code));
+//   useEffect(() => {
+//     let code = new URL(window.location.href).searchParams.get("code");
+//     userActions(code);
+//   }, []);
+
+//   return <Spinner />;
+// };
+
+// export default KaKaoLoginHandler;
+import React, { useEffect } from "react";
+import { kakaoLogin as userActions } from "./../../redux/modules/user.js";
+import Spinner from "./Spinner.jsx";
+
+const OAuth2RedirectHandler = (props) => {
+  // 인가코드
+  useEffect(() => {
+    let code = new URL(window.location.href).searchParams.get("code");
+    userActions(code);
   }, []);
 
   return <Spinner />;
 };
 
-export default KaKaoLoginHandler;
+export default OAuth2RedirectHandler;

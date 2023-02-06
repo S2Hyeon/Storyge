9f644a97f68583c9c41240d589e78b21c7f3d3f9
안태현 <dksxogus1012@gmail.com>
2023-02-06 11:31:08 +0900

    [FE] Refactor : 필요없는 파라미터와 태그 삭제

--- Storyge/front-end/src/components/calender/Calendar.jsx
+++ Storyge/front-end/src/components/calender/Calendar.jsx
@@ -9,35 +9,25 @@ function CustomCalendar() {
   const [emotionData] = useState(datas);
 
   return (
-    <>
-      <Calendar
-        calendarType="US"
-        showNeighboringMonth={false} //  이전, 이후 달의 날짜는 보이지 않도록 설정
-        formatDay={(locale, date) => dayjs(date).format("D")}
-        tileContent={({ date, view }) => {
-          let chosenData = emotionData.find(
-            ({ feelDate }) => feelDate === dayjs(date).format("YYYY-MM-DD")
-          );
+    <Calendar
+      calendarType="US"
+      showNeighboringMonth={false} //  이전, 이후 달의 날짜는 보이지 않도록 설정
+      formatDay={(locale, date) => dayjs(date).format("D")}
+      tileContent={({ date, view }) => {
+        let chosenData = emotionData.find(
+          ({ feelDate }) => feelDate === dayjs(date).format("YYYY-MM-DD")
+        );
 
-          //해당 일자에 데이터가 있다면 그 감정을 props로 넘김
-          if (chosenData != null) {
-            return (
-              <Emoji
-                emotion={chosenData.emotion}
-                thisWidth="80%"
-                thisHeight="80px"
-              />
-            );
-          }
-          //해당 일자에 데이터가 없다면 noemotion을 props로 넘기기
-          else {
-            return (
-              <Emoji emotion="noemotion" thisWidth="80%" thisHeight="80px" />
-            );
-          }
-        }}
-      />
-    </>
+        //해당 일자에 데이터가 있다면 그 감정을 props로 넘김
+        if (chosenData != null) {
+          return <Emoji emotion={chosenData.emotion} thisWidth="80%" />;
+        }
+        //해당 일자에 데이터가 없다면 noemotion을 props로 넘기기
+        else {
+          return <Emoji emotion="noemotion" thisWidth="80%" />;
+        }
+      }}
+    />
   );
 }
 

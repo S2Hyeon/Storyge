b507416cdf485d5b72c4665605504a422cfbf4c0
두소원 <sowondoo@naver.com>
2023-02-08 09:40:58 +0900

    [BE] Feat: swagger

--- Storyge/Back-End/src/main/java/com/example/project/notification/controller/NotificationController.java
+++ Storyge/Back-End/src/main/java/com/example/project/notification/controller/NotificationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.List;
 
@@ -32,7 +33,7 @@ public class NotificationController {
     //알림 목록
     @ApiOperation(value = "알림 목록 조회", notes = "사용자 알림 목록 리스트 가져옴")
     @GetMapping("/notification")
-    public ResponseEntity<List<NotificationReponseDto>> selectAllNotification(@RequestHeader(name = TOKEN_PREFIX) String token){
+    public ResponseEntity<List<NotificationReponseDto>> selectAllNotification(@ApiIgnore @RequestHeader(name = TOKEN_PREFIX) String token){
 
         Long userId = jwtUtil.getUserId(token);
 

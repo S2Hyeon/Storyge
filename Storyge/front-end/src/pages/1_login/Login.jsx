2052a1c66596f0012b43936565838b586f751551
송수현 <ssh1692@naver.com>
2023-02-08 17:36:43 +0900

    [FE] Design : Login 디자인 버튼 위치 수정

--- Storyge/front-end/src/pages/1_login/Login.jsx
+++ Storyge/front-end/src/pages/1_login/Login.jsx
@@ -9,14 +9,26 @@ import {
 export default function Login() {
   return (
     <S.Login>
-      <h1>Login</h1>
+      <S.LoginText>Login</S.LoginText>
 
-      <S.Google
-        href={GOOGLE_AUTH_URL}
-        onClick={() => alert("google")}
-      ></S.Google>
-      <S.Naver href={NAVER_AUTH_URL} onClick={() => alert("naver")}></S.Naver>
-      <S.Kakao href={KAKAO_AUTH_URL} onClick={() => alert("kakao")}></S.Kakao>
+      <S.LinkBtn href={GOOGLE_AUTH_URL} onClick={() => alert("google")}>
+        <S.GoogleImg
+          src="https://t1.daumcdn.net/cfile/tistory/998689465C3D7D1217?original"
+          alt="naver"
+        ></S.GoogleImg>
+      </S.LinkBtn>
+      <S.LinkBtn href={NAVER_AUTH_URL} onClick={() => alert("naver")}>
+        <S.NaverImg
+          src="https://t1.daumcdn.net/cfile/tistory/99580C465C3D7D130C?original"
+          alt="naver"
+        ></S.NaverImg>
+      </S.LinkBtn>
+      <S.LinkBtn href={KAKAO_AUTH_URL} onClick={() => alert("kakao")}>
+        <S.KakaoImg
+          src="https://t1.daumcdn.net/cfile/tistory/99BEE8465C3D7D1214?original"
+          alt="kakao"
+        ></S.KakaoImg>
+      </S.LinkBtn>
     </S.Login>
   );
 }

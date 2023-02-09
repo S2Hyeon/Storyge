b84de10e94fe00f9947ef0e822f71a8e53754d0d
jjackdduke <tjgmldus100@gmail.com>
2023-02-09 23:21:17 +0900

    [FE] Feat : 노래 추천 기능 완성

--- Storyge/front-end/src/pages/8_music/Music.jsx
+++ Storyge/front-end/src/pages/8_music/Music.jsx
@@ -1,43 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, Suspense } from "react";
 import * as G from "../../styles";
 import * as S from "./Music.js";
 import { OpenAI } from "../../openai/OpenAI";
 import MusicResult from "./MusicResult";
 import axios from "axios";
+import Spinner from "../../components/spinner/Spinner"
+import ReactPlayer from "react-player";
+
 // import { reject } from "q";
 // import { resolve } from "path";
 
 export default function Music() {
-  const [url, setUrl] = useState("");
-  const [result, setResult] = useState("분석전");
+  const [url, setUrl] = useState(null);
   const [content, setContent] = useState("");
   const [youtubeOpen, setYoutubeOpen] = useState(false);
-  const [videoId, setVideoId] = useState();
+  // const [videoId, setVideoId] = useState();
   async function findMusic() {
     const title = await OpenAI({ input: content, type: 0 });
-    setResult(title + " lylics");
+    const result = title + " lylics"
     axios({
       method: "get",
-      url: "https://www.googleapis.com/youtube/v3/search",
+      url: "https://www.googleapis.com/youtube/v3/search?",
       params: {
         key: process.env.REACT_APP_YOUTUBE_API_KEY,
+        part: "snippet",
         q: result,
       },
     })
       .then((res) => {
         console.log(res);
-        setVideoId(res.data.items[0].id.videoId);
-        setUrl(`https://www.youtube.com/watch?v=${videoId}`);
+        // setVideoId(res.data.items[0].id.videoId);
+        setUrl(`https://www.youtube.com/watch?v=${res.data.items[0].id.videoId}`);
+        setYoutubeOpen(true);
       })
       .catch((err) => {
         console.log(err);
       });
-    setYoutubeOpen(true);
   }
-
-  // function gomusicresult() {
-  //   movePage("/musicresult");
-  // }
   return (
     <G.BodyContainer>
       <S.Rectangle
@@ -50,9 +49,18 @@ export default function Music() {
       <G.longBtnDefault onClick={findMusic} style={{ marginBottom: "20px" }}>
         <S.Text>분석하기</S.Text>
       </G.longBtnDefault>
-      <div>{result}</div>
-      <div>{videoId}</div>
-      {youtubeOpen && <MusicResult path={url} />}
+      {!youtubeOpen ? (
+      <Spinner />
+      ) : (    <G.BodyContainer>
+        {/* Youtube Player 자동실행 */}
+        <ReactPlayer
+          url={url}
+          width="300px"
+          height="200px"
+          playing={true}
+          style={{ margin: "auto" }}
+        />
+      </G.BodyContainer>)}
     </G.BodyContainer>
   );
 }

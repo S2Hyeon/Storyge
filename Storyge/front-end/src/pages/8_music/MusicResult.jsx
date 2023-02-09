d247df5a8e23b9fd18097aeb3f1c9311830a341a
서희연 <tjgmldus100@naver.com>
2023-02-09 17:57:35 +0900

    [FE] Feat : youtube api 연결 진행중

--- Storyge/front-end/src/pages/8_music/MusicResult.jsx
+++ Storyge/front-end/src/pages/8_music/MusicResult.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import * as G from './../../styles/index';
-import * as S from './../8_music/Music.js'
-import ReactPlayer from 'react-player'
+import React from "react";
+import * as G from "./../../styles/index";
+import * as S from "./../8_music/Music.js";
+import ReactPlayer from "react-player";
 
-export default function MusicResult({ link }) {
+export default function MusicResult(path) {
   return (
     <G.BodyContainer>
       {/* Youtube Player 자동실행 */}
-      <ReactPlayer url='https://www.youtube.com/watch?v=a7Xl-eeSFuc' width='300px' height='200px' playing='true'
-      style={{margin: 'auto'}}/>
+      <ReactPlayer
+        url={path}
+        width="300px"
+        height="200px"
+        playing={true}
+        style={{ margin: "auto" }}
+      />
     </G.BodyContainer>
   );
 }
-

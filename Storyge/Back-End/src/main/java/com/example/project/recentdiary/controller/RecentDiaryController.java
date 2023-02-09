1390862390abc8dc69bd4ae9726e1aab81dbf4bf
박수민 <1379psm@naver.com>
2023-02-09 16:06:03 +0900

    [BE] Feat recentDiary 수정

--- Storyge/Back-End/src/main/java/com/example/project/recentdiary/controller/RecentDiaryController.java
+++ Storyge/Back-End/src/main/java/com/example/project/recentdiary/controller/RecentDiaryController.java
@@ -14,27 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
-import static com.example.project.user.model.jwt.JwtProperties.*;
+import static com.example.project.user.model.jwt.JwtProperties.TOKEN_HEADER;
 
 @RestController
 @RequiredArgsConstructor
 @Api(tags = {"팔로잉의 최근 일기(스토리) API"})
 public class RecentDiaryController {
 
-    private final RecentDiaryService recentDiaryService;
     private static final String SUCCESS = "Success";
     private static final String FAIL = "Fail";
+    private final RecentDiaryService recentDiaryService;
     private final JwtUtil jwtUtil;
 
     @GetMapping("/recent")
     @ApiOperation(value = "팔로잉 최근 다이어리(스토리)", notes = "팔로잉들의 가장 최근 일기 중, 공개이면서 읽지 않은 일기 목록 반환")
-    public ResponseEntity<?> selectAllRecentDiary(HttpServletRequest request){
+    public ResponseEntity<?> selectAllRecentDiary(HttpServletRequest request) {
 
         String token = request.getHeader(TOKEN_HEADER);
         Long userId = jwtUtil.getUserId(token);
-
         List<RecentDiaryResponseDto> recentDiaryList = recentDiaryService.selectAllRecentDiary(userId);
-        if(recentDiaryList==null){
+        if (recentDiaryList == null) {
             return new ResponseEntity<>("팔로잉 없음", HttpStatus.OK);
         }
 

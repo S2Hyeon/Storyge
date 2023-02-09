32b6ae64749a485207ce3ab7f12a682d311143a3
안태현 <dksxogus1012@gmail.com>
2023-02-09 23:29:20 +0900

    [FE] Refactor : customApi 수정

--- Storyge/front-end/src/lib/customApi.js
+++ Storyge/front-end/src/lib/customApi.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+
+
 const Api = axios.create({
   baseURL: "https://storyge.xyz/api",
   // timeout: 10000,
-  // params: {},
+  params: {},
 });
 
 export default Api;

8de24d1e3d12c4bcbeb50f459e6594928c3d89f1
songsuhyeon <ssh1692@naver.com>
2023-02-12 21:47:25 +0900

    [FE] Design : Error 페이지 이모지 변경

--- Storyge/front-end/src/pages/9_errorPage/ErrorPage.jsx
+++ Storyge/front-end/src/pages/9_errorPage/ErrorPage.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import * as G from "./../../styles/index";
 import * as S from "./ErrorPageStyle";
 import ErrorIcon from "../../assets/errorIcon.png";
+import CryLottie from "../../api/animation/CryLottie.jsx";
 
 function ErrorPage(props) {
   return (
     <G.BodyContainer>
         <S.Container>
-          <S.Img src={ErrorIcon} alt="404 error image" />
+        {/* <S.Img src={ErrorIcon} alt="404 error image" /> */}
+          <CryLottie />
           <S.Oops>Oops!</S.Oops>
-        <S.Text>{ props.text === "404" ? "없는 페이지입니다.": "네트워크 오류입니다."}</S.Text>
+        <S.Text>{ props.text === "404" ? `유효한 주소를 입력해주세요.`: "네트워크 오류입니다."}</S.Text>
       </S.Container>
     </G.BodyContainer>
       

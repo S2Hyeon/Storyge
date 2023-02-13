f733545cb67f5fad75b74168bbd594b6b85b84c3
박수민 <1379psm@naver.com>
2023-02-13 15:05:35 +0900

    [BE] Feat: fileupload 구현

---
+++ Storyge/Back-End/src/main/java/com/example/project/user/model/file/FileServiceImpl.java
@@ -0,0 +1,76 @@
+package com.example.project.user.model.file;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class FileServiceImpl implements FileService {
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+
+        File uploadFile = convert(multipartFile)
+                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File로 전환이 실패했습니다."));
+
+//        return upload(userId, param.getNickname(), uploadFile, dirName);
+        return upload(uploadFile, dirName);
+    }
+
+    private String upload(File uploadFile, String dirName) {
+        String fileName = dirName + "/" + uploadFile.getName();
+        String uploadImageUrl = putS3(uploadFile, fileName);
+        removeNewFile(uploadFile); // 로컬에 생성된 File 삭제 (MultipartFile -> File 전환 하며 로컬에 파일 생성됨)
+
+//FileUploadResponse DTO로 반환해준다.
+        return uploadImageUrl; // 업로드 된 파일의 S3 URL 주소 반환
+    }
+
+    private String putS3(File uploadFile, String fileName) {
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(
+                CannedAccessControlList.PublicRead));
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+    private void removeNewFile(File targetFile) {
+        if (targetFile.delete()) {
+            log.info("파일이 삭제되었습니다.");
+        } else {
+            log.info("파일이 삭제되지 못했습니다.");
+        }
+    }
+
+    private Optional<File> convert(MultipartFile file) throws IOException {
+        System.out.println("convert 파일");
+        File convertFile = new File(file.getOriginalFilename());
+        System.out.println("getOriginalFilename(): " + file.getOriginalFilename());
+        System.out.println("convertFile: " + convertFile.getName());
+        if (convertFile.createNewFile()) {
+            System.out.println("if 안에 들어옴?");
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+                System.out.println("try 안에 들어옴?");
+                fos.write(file.getBytes());
+            }
+
+            System.out.println("return 전임?");
+            return Optional.of(convertFile);
+        }
+
+        return Optional.empty();
+    }
+}

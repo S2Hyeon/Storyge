5ee62fdcfff3b4881af5a43c3dc76e7348d8e742
송수현 <ssh1692@naver.com>
2023-02-13 12:35:14 +0900

    [FE] Refactor : 접속 가능한 기기 height 변경

--- Storyge/front-end/src/App.js
+++ Storyge/front-end/src/App.js
@@ -1,17 +1,17 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./router/AppRouter";
-import { debounce } from 'lodash';
-import Size from 'pages/0_intro/Size.jsx';
-import ErrorPage from 'pages/9_errorPage/ErrorPage';
+import { debounce } from "lodash";
+import Size from "pages/0_intro/Size.jsx";
+import ErrorPage from "pages/9_errorPage/ErrorPage";
 
 function App() {
   const [isConnected, setIsConnected] = useState(false);
 
   const [windowSize, setWindowSize] = useState({
     width: window.innerWidth,
-    height: window.innerHeight
+    height: window.innerHeight,
   });
 
   // handleResize 함수를 debounce로 감싸고, 시간을 설정한다
@@ -19,15 +19,16 @@ function App() {
   const handleResize = debounce(() => {
     setWindowSize({
       width: window.innerWidth,
-      height: window.innerHeight
+      height: window.innerHeight,
     });
   }, 100);
- 
+
   useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    return () => { // cleanup 
-      window.removeEventListener('resize', handleResize);
-    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      // cleanup
+      window.removeEventListener("resize", handleResize);
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -42,10 +43,17 @@ function App() {
 
   return (
     <div className="App">
-      {isConnected ?       <BrowserRouter>
-        {(windowSize.width < 440 && windowSize.height < 900) ? <AppRouter /> : <Size width={ window.width} height={ window.height} />}
-      </BrowserRouter> : <ErrorPage text="network"/>}
-
+      {isConnected ? (
+        <BrowserRouter>
+          {windowSize.width < 440 && windowSize.height < 950 ? (
+            <AppRouter />
+          ) : (
+            <Size width={window.width} height={window.height} />
+          )}
+        </BrowserRouter>
+      ) : (
+        <ErrorPage text="network" />
+      )}
     </div>
   );
 }

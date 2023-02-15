f4cb88b173199fdbcadab30c2cab831be2e2d945
songsuhyeon <ssh1692@naver.com>
2023-02-16 04:21:01 +0900

    [FE] Refactor : Nav, Header console.log 삭제

--- Storyge/front-end/src/common/footer/Nav.jsx
+++ Storyge/front-end/src/common/footer/Nav.jsx
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import * as S from "./NavStyle";
-import { TbHome, TbMusic, TbPlus, TbBell, TbUser } from "react-icons/tb";
-import { EventSourcePolyfill } from "event-source-polyfill";
-import { getCookie } from "utils/Cookies";
+import React, { useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import * as S from './NavStyle'
+import { TbHome, TbMusic, TbPlus, TbBell, TbUser } from 'react-icons/tb'
+import { EventSourcePolyfill } from 'event-source-polyfill'
+import { getCookie } from 'utils/Cookies'
 
 function Footer(props) {
-  const location = useLocation();
-  const movePage = useNavigate();
+  const location = useLocation()
+  const movePage = useNavigate()
 
   //알림 실시간 처리
-  const [isNewAlert, setIsNewAlert] = useState(false);
-  const eventSource = new EventSourcePolyfill("https://storyge.xyz/api/sub", {
+  const [isNewAlert, setIsNewAlert] = useState(false)
+  const eventSource = new EventSourcePolyfill('https://storyge.xyz/api/sub', {
     headers: {
-      Authorization: getCookie("token"),
+      Authorization: getCookie('token'),
       // "Content-Type": "text/evnent-stream",
       // "Cache-Control": "no-cache",
       // Connection: "keep-alive",
@@ -21,44 +21,42 @@ function Footer(props) {
     },
     heartbeatTimeout: 1200000,
     withCredentials: true,
-  });
-  eventSource.addEventListener("connect", (e) => {
-    const { data: receivedConnectData } = e;
-    console.log("connected?", receivedConnectData);
-  });
-  eventSource.addEventListener("notification", function(event) {
-    const { data: receivedConnectData } = event;
-    console.log("noti?", receivedConnectData);
-    setIsNewAlert(true);
-  });
+  })
+  eventSource.addEventListener('connect', (e) => {
+    const { data: receivedConnectData } = e
+  })
+  eventSource.addEventListener('notification', function (event) {
+    const { data: receivedConnectData } = event
+    setIsNewAlert(true)
+  })
 
   return (
     <S.Nav>
       <S.IconContainer
         onClick={() => {
-          movePage("/");
+          movePage('/')
         }}
       >
         <TbHome
           size={30}
-          color={location.pathname === "/" ? "var(--color-primary)" : "#D9D9D9"}
+          color={location.pathname === '/' ? 'var(--color-primary)' : '#D9D9D9'}
         />
       </S.IconContainer>
       <S.IconContainer
         onClick={() => {
-          movePage("/music");
+          movePage('/music')
         }}
       >
         <TbMusic
           size={30}
           color={
-            location.pathname === "/music" ? "var(--color-primary)" : "#D9D9D9"
+            location.pathname === '/music' ? 'var(--color-primary)' : '#D9D9D9'
           }
         />
       </S.IconContainer>
       <S.IconContainer
         onClick={() => {
-          movePage("/createDiary");
+          movePage('/createDiary')
         }}
       >
         <S.CenterCircle>
@@ -67,43 +65,43 @@ function Footer(props) {
       </S.IconContainer>
       <S.IconContainer
         onClick={() => {
-          movePage("/alarm");
-          setIsNewAlert(false);
+          movePage('/alarm')
+          setIsNewAlert(false)
         }}
-        style={{ position: "relative" }}
+        style={{ position: 'relative' }}
       >
-        <div style={{ position: "absolute" }}>
+        <div style={{ position: 'absolute' }}>
           <TbBell
             size={30}
             color={
-              location.pathname === "/alarm"
-                ? "var(--color-primary)"
-                : "#D9D9D9"
+              location.pathname === '/alarm'
+                ? 'var(--color-primary)'
+                : '#D9D9D9'
             }
           />
         </div>
         {isNewAlert ? (
-          <S.AlertContainer style={{ position: "absolute" }}>
+          <S.AlertContainer style={{ position: 'absolute' }}>
             <S.Alert />
           </S.AlertContainer>
         ) : null}
       </S.IconContainer>
       <S.IconContainer
         onClick={() => {
-          movePage("/mypage");
+          movePage('/mypage')
         }}
       >
         <TbUser
           size={30}
           color={
-            location.pathname === "/mypage" || location.pathname === "/follower"
-              ? "var(--color-primary)"
-              : "#D9D9D9"
+            location.pathname === '/mypage' || location.pathname === '/follower'
+              ? 'var(--color-primary)'
+              : '#D9D9D9'
           }
         />
       </S.IconContainer>
     </S.Nav>
-  );
+  )
 }
 
-export default Footer;
+export default Footer

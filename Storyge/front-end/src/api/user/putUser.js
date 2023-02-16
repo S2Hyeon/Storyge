fc60881662c3a17d6134bec5ce212e67a15c3d9a
안태현 <dksxogus1012@gmail.com>
2023-02-16 09:07:59 +0900

    [FE] Fix : putUser 괄호 에러 수정

--- Storyge/front-end/src/api/user/putUser.js
+++ Storyge/front-end/src/api/user/putUser.js
@@ -1,27 +1,19 @@
-import axios from 'axios'
-import Api from 'lib/customApi'
-import { getCookie } from 'utils/Cookies'
 import Api from "lib/customApi";
 import { getCookie } from "utils/Cookies";
 
 export async function putUser(file, nickName) {
   try {
-    const formData = new FormData()
-    const blob = new Blob([nickName], { type: 'application/json' })
-    formData.append('multipartFile', file)
-    formData.append('nickname', blob)
-    const response = await axios.put('http://localhost:8080/user', formData, {
     const formData = new FormData();
     const blob = new Blob([nickName], { type: "application/json" });
     formData.append("multipartFile", file);
     formData.append("nickname", blob);
-    await Api.put("/user", formData, {
+    const response = await Api.put("/user", formData, {
       headers: {
-        Authorization: getCookie('token'),
-        'Content-Type': 'multipart/form-data',
+        Authorization: getCookie("token"),
+        "Content-Type": "multipart/form-data",
       },
-    })
+    });
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }

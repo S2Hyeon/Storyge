bd8fb7d8aab42b0f893826358e94d3bbf3d740d0
안태현 <dksxogus1012@gmail.com>
2023-02-17 09:25:12 +0900

    [FE] Fix : 구글,네이버 로그인 시 프로필 수정 페이지로 넘어가게, 확대금지

--- Storyge/front-end/src/router/Layout.jsx
+++ Storyge/front-end/src/router/Layout.jsx
@@ -5,19 +5,19 @@ import Footer from "../common/footer/Nav";
 
 function Layout() {
   //화면크기가 작아지면 Footer 사라지게
-  const [windowHeight, setWindowHeight] = useState(window.innerHeight);
-  const windowWidth = window.innerWidth;
-  const [ratio, setRatio] = useState(windowWidth / windowHeight);
-  window.onresize = function(event) {
-    setWindowHeight(window.innerHeight);
-    setRatio(windowWidth / windowHeight);
-  };
+  // const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+  // const windowWidth = window.innerWidth;
+  // const [ratio, setRatio] = useState(windowWidth / windowHeight);
+  // window.onresize = function(event) {
+  //   setWindowHeight(window.innerHeight);
+  //   setRatio(windowWidth / windowHeight);
+  // };
 
   return (
     <div>
       <Header />
       <Outlet />
-      {ratio < 5 / 7 ? <Footer /> : null}
+      <Footer />
     </div>
   );
 }
